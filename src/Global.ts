14472ecd07daaf721f284b68a74f8d0a51036e66
PatBG <pat@patbgames.com>
2023-10-08 09:09:10 +0200

    Game state is now accessible by an event and a listener

--- src/Global.ts
+++ src/Global.ts
@@ -1,7 +1,15 @@
 import * as Phaser from 'phaser';
 import { Bullets } from './Bullets';
 
-export class Global { // abstract
+export enum GameState {
+    Intermission,
+    GotoShop,
+    Shop,
+    Fight
+}
+
+export class Global {
+    
     public static bullets: Bullets;
     public static enemyBullets: Bullets;
     public static explosionPlayer: Phaser.GameObjects.Particles.ParticleEmitter;
@@ -9,4 +17,18 @@ export class Global { // abstract
     public static canvasWidth: number;
     public static canvasHeight: number;
     public static cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+
+    // Game state
+    private static gameState: GameState = GameState.Intermission;
+    private static gameStateEmitter: Phaser.Events.EventEmitter = new Phaser.Events.EventEmitter();
+    public static onGameStateChange(callback: (state: GameState) => void): void {
+        this.gameStateEmitter.on('change', callback);
+    }
+    public static setGameState(state: GameState): void {
+        this.gameState = state;
+        this.gameStateEmitter.emit('change', state);
+    }
+    public static getGameState(): GameState {
+        return this.gameState;
+    }
 }
