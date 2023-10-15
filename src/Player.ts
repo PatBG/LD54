123c818dfa5b603bece2c489c1ad0cc9c5d962b7
PatBG <pat@patbgames.com>
2023-10-15 12:51:36 +0200

    Added keyboard/mouse management for player inputs

--- src/Player.ts
+++ src/Player.ts
@@ -2,13 +2,18 @@ import * as Phaser from 'phaser';
 import { GameState, Global } from './Global';
 import { Module, Modules, ModuleType } from './Modules';
 import { Bullets } from './Bullets';
+import { Bounds } from 'matter';
 
 
 export class Player extends Phaser.GameObjects.Container {
-    speed = 300;
+    speed = 200;
     modules: Modules;
     bullets: Bullets;
     private static singleton: Player;
+
+    soundPlayerShield: Phaser.Sound.BaseSound;
+    PlayerFire: Phaser.Sound.BaseSound;
+    PlayerExplosion: Phaser.Sound.BaseSound;
 
     constructor(scene: Phaser.Scene, x: number, y: number, bullets: Bullets) {
         super(scene, x, y);
@@ -20,24 +25,13 @@ export class Player extends Phaser.GameObjects.Container {
         // this.body.setCollideWorldBounds(true);
         Global.cursorKeys = this.scene.input.keyboard.createCursorKeys();
 
-        this.add(this.newStructure(0, 0));
-
-        this.add(this.newStructure(-1, 1));
-        this.add(this.newStructure(0, 1));
-        this.add(this.newStructure(1, 1));
-
         this.modules = this.scene.add.existing(new Modules(this.scene.physics.world, this.scene, { name: 'modulesContainer' }, this.bullets));
-        this.add(this.modules.newModule(0, 0, ModuleType.Cannon));
-
-        this.add(this.modules.newModule(-1, 1, ModuleType.Defense));
-        this.add(this.modules.newModule(0, 1, ModuleType.Merchandise));
-        this.add(this.modules.newModule(1, 1, ModuleType.Defense));
 
         Global.onGameStateChange((state: GameState) => { this.onGameStateChange(state); });
     }
 
     onGameStateChange(state: GameState) {
-        if (state === GameState.GoToShop) {
+        if (state === GameState.EndWave) {
             // Stop the ship
             this.body.velocity.x = 0;
             this.body.velocity.y = 0;
@@ -48,7 +42,8 @@ export class Player extends Phaser.GameObjects.Container {
                 x: Global.PlayerPosInShop.x,
                 y: Global.PlayerPosInShop.y,
                 ease: 'Linear',
-                duration: Math.sqrt(Math.pow(this.x - Global.PlayerPosInShop.x, 2) + Math.pow(this.y - Global.PlayerPosInShop.y, 2)) / this.speed * 1000,
+                duration: Math.sqrt(Math.pow(this.x - Global.PlayerPosInShop.x, 2)
+                    + Math.pow(this.y - Global.PlayerPosInShop.y, 2)) / this.speed * 1000,
                 onComplete: () => {
                     Global.setGameState(GameState.Shop);
                 },
@@ -57,47 +52,91 @@ export class Player extends Phaser.GameObjects.Container {
         else if (state === GameState.Fight) {
             this.modules.onBeginWave();
         }
+        else if (state === GameState.GameStart) {
+            // Initialisation of the player ship
+            this.x = Global.PlayerPosInShop.x;
+            this.y = Global.PlayerPosInShop.y;
+
+            this.removeAllStructures();
+            this.add(this.newStructure(0, 0));
+            this.add(this.newStructure(-1, 1));
+            this.add(this.newStructure(0, 1));
+            this.add(this.newStructure(1, 1));
+
+            this.add(this.modules.newModule(0, 0, ModuleType.Cannon));
+            this.add(this.modules.newModule(-1, 1, ModuleType.Defense));
+            this.add(this.modules.newModule(0, 1, ModuleType.Merchandise));
+            this.add(this.modules.newModule(1, 1, ModuleType.Defense));
+        }
+        else if (state === GameState.GameOver) {
+            this.body.velocity.x = 0;
+            this.body.velocity.y = 0;
+        }
     }
 
+    inputIsKey = true;
+    inputPrecPointer = new Phaser.Math.Vector2(0, 0);
     update(time, delta) {
         if (Global.getGameState() !== GameState.Fight) return;
 
+        if (this.modules.countActive() === 0) {
+            Global.setGameState(GameState.GameOver);
+            return;
+        }
+
+        // Update modules
         this.modules.update(time, delta);
 
-        if (Global.cursorKeys.left.isDown) {
-            this.body.velocity.x = -this.speed;
+        if (Global.cursorKeys.left.isDown || Global.cursorKeys.right.isDown
+            || Global.cursorKeys.up.isDown || Global.cursorKeys.down.isDown
+            || !this.scene.input.isOver) {
+            this.inputIsKey = true;
         }
-        else if (Global.cursorKeys.right.isDown) {
-            this.body.velocity.x = this.speed;
-        }
-        else {
-            this.body.velocity.x = 0;
-        }
-
-        if (Global.cursorKeys.up.isDown) {
-            this.body.velocity.y = -this.speed;
-        }
-        else if (Global.cursorKeys.down.isDown) {
-            this.body.velocity.y = this.speed;
-        }
-        else {
-            this.body.velocity.y = 0;
+        else if (this.inputPrecPointer.x != this.scene.input.activePointer.x
+            || this.inputPrecPointer.y != this.scene.input.activePointer.y) {
+            this.inputPrecPointer.x = this.scene.input.activePointer.x;
+            this.inputPrecPointer.y = this.scene.input.activePointer.y;
+            this.inputIsKey = false;
         }
 
-        // this.hackUnitTest();
-    }
-
-    hackFirstCall = true;
-    hackUnitTest() {                                    // HACK: unit test
-        if (this.hackFirstCall) {
-            this.hackFirstCall = false;
-            for (let x = -2; x <= 2; x++) {             // Unit test : isStructure() and getModule()
-                for (let y = 0; y <= 3; y++) {
-                    console.log(`Structure [${x},${y}]=${this.isStructure(x, y)}`);
-                    const module = this.modules.getModule(x, y);
-                    console.log(`Modules [${x},${y}]=${(module === undefined) ? "undefined" : ModuleType[module.moduleType]}`);
-                }
+        if (!this.inputIsKey) {
+            const mouse = this.scene.input.activePointer;
+            const playerRect = new Phaser.Geom.Rectangle(this.x - 8, this.y - 8, 16, 16);
+            // if the mouse pointer is on the scene and not over the center ship, move the ship
+            if (!playerRect.contains(mouse.x, mouse.y)) {
+                const angle = Phaser.Math.Angle.Between(this.x, this.y, this.scene.input.activePointer.x, this.scene.input.activePointer.y);
+                this.body.velocity.x = Math.cos(angle) * this.speed;
+                this.body.velocity.y = Math.sin(angle) * this.speed;
             }
+            else {
+                this.body.velocity.x = 0;
+                this.body.velocity.y = 0;
+            }
+        }
+        else {
+            let v = new Phaser.Math.Vector2(0, 0);
+            if (Global.cursorKeys.left.isDown) {
+                v.x = -this.speed;
+            }
+            else if (Global.cursorKeys.right.isDown) {
+                v.x = this.speed;
+            }
+
+            if (Global.cursorKeys.up.isDown) {
+                v.y = -this.speed;
+            }
+            else if (Global.cursorKeys.down.isDown) {
+                v.y = this.speed;
+            }
+
+            // Diagonal movement : normalize speed
+            if (v.x !== 0 && v.y !== 0) {
+                v.x *= 0.7071;
+                v.y *= 0.7071;
+            }
+
+            this.body.velocity.x = v.x;
+            this.body.velocity.y = v.y;
         }
     }
 
@@ -120,7 +159,7 @@ export class Player extends Phaser.GameObjects.Container {
     }
 
     isStructure(x: number, y: number): boolean {
-        return this.getStructure(x,y) !== undefined;
+        return this.getStructure(x, y) !== undefined;
     }
 
     getStructure(x: number, y: number): Phaser.GameObjects.Sprite | undefined {
@@ -162,7 +201,7 @@ export class Player extends Phaser.GameObjects.Container {
 
     static NbModule(): number {
         if (Player.singleton !== undefined) {
-                return Player.singleton.modules.getLength();
+            return Player.singleton.modules.getLength();
         }
         return 0;
     }
@@ -176,5 +215,9 @@ export class Player extends Phaser.GameObjects.Container {
             }
         }
         return false;
+    }
+
+    removeAllStructures() {
+        this.each((structure: Phaser.GameObjects.Sprite) => { this.remove(structure, true); }, this);
     }
 }
