225072ff22a367141955e75588ba9c899687cbef
PatBG <pat@patbgames.com>
2023-10-15 21:06:22 +0200

    Added EnemyFlyingSaucer sound for enemy3

--- src/Sounds.ts
+++ src/Sounds.ts
@@ -3,14 +3,15 @@ import * as Phaser from 'phaser';
 export class Sounds {
 
     public static EnemyExplosion: Phaser.Sound.BaseSound;
+    public static EnemyFire: Phaser.Sound.BaseSound;
     public static PlayerFire: Phaser.Sound.BaseSound;
     public static PlayerExplosion: Phaser.Sound.BaseSound;
     public static PlayerShield: Phaser.Sound.BaseSound;
-    public static EnemyFire: Phaser.Sound.BaseSound;
 
     public static preload(scene: Phaser.Scene) {
         scene.load.audio('EnemyExplosion', 'assets/audio/EnemyExplosion.wav');
         scene.load.audio('EnemyFire', 'assets/audio/EnemyFire.wav');
+        scene.load.audio('EnemyFlyingSaucer', 'assets/audio/EnemyFlyingSaucer.wav');        
         scene.load.audio('PlayerFire', 'assets/audio/PlayerFire.wav');
         scene.load.audio('PlayerExplosion', 'assets/audio/PlayerExplosion.wav');
         scene.load.audio('PlayerShield', 'assets/audio/PlayerShield.wav');
