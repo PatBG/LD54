6715488122abb3f5f097da8d23c2cc43128c40a4
PatBG <pat@patbgames.com>
2023-11-01 23:38:23 +0100

    Separation of Modules and Module class

--- src/Modules.ts
+++ src/Modules.ts
@@ -1,118 +1,13 @@
 import * as Phaser from 'phaser';
 import { GameManager } from './GameManager';
 import { Bullets } from './Bullets';
-import { SoundManager } from './SoundManager';
-import { PlayerManager } from './PlayerManager';
+import { Module } from './Module';
+
 
 export enum ModuleType {
     Merchandise = 1,
     Cannon,
     Defense,
-}
-
-export class Module extends Phaser.Physics.Arcade.Sprite {
-    moduleType: ModuleType;
-    level = 1;
-    life = 1;
-    bullets: Bullets;
-    timeNextFire = 0;
-    angleCannon = 0;
-    tweenLowLife: Phaser.Tweens.Tween;
-
-    constructor(scene: Phaser.Scene, x: number, y: number, key: string, moduleType: ModuleType) {
-        super(scene, x, y, key, moduleType);
-        this.moduleType = moduleType;
-    }
-
-    onCreate(bullets: Bullets) {
-        if (this.moduleType === ModuleType.Cannon) {
-            this.bullets = bullets;
-        }
-    }
-
-    onBeginWave() {
-        // Initialize life
-        if (this.moduleType === ModuleType.Defense) {
-            this.life = 1 + this.level;
-        }
-        else {
-            this.life = 2;
-        }
-    }
-
-    onEndWave() {
-        // Upgrade alive merchandises at the end of every wave 
-        if (this.moduleType === ModuleType.Merchandise) {
-            this.level++;
-        }
-        // Remove low life effect if set
-        if (this.tweenLowLife != undefined) {
-            this.tweenLowLife.remove();
-            this.alpha = 1;                             // Reset alpha to 1
-        }
-    }
-
-    onHit() {
-        this.life--;
-        if (this.life == 1) {
-            this.tweenLowLife = this.scene.tweens.add({
-                targets: this,
-                alpha: 0.3,
-                ease: 'Linear',
-                duration: 250,
-                yoyo: true,
-                repeat: -1,
-            });
-            SoundManager.getInstance().PlayerShield.play();
-        }
-        else if (this.life <= 0) {
-            this.onDestroy();
-            SoundManager.getInstance().PlayerExplosion.play();
-        }
-    }
-
-    isAlive(): boolean {
-        return this.life > 0;
-    }
-
-    onDestroy() {
-        if (this.tweenLowLife != undefined) {
-            this.tweenLowLife.remove();
-            this.tweenLowLife.destroy();
-        }
-        this.destroy();
-    }
-
-    addAngleCannon(angleCannon: number) {
-        if (this.moduleType === ModuleType.Cannon) {
-            this.angleCannon += angleCannon;
-            this.setRotation(this.angleCannon);
-        }
-    }
-
-    setAngleCannon(angleCannon: number) {
-        if (this.moduleType === ModuleType.Cannon) {
-            this.angleCannon = angleCannon;
-            this.setRotation(angleCannon);
-        }
-    }
-
-    update(time, delta) {
-        if (this.moduleType === ModuleType.Cannon) {
-            if (time >= this.timeNextFire) {
-                this.timeNextFire = time + 1000 / PlayerManager.getInstance().cannonFireRate(this.level);
-                this.onFire();
-            }
-        }
-    }
-
-    onFire() {
-        const velocity = PlayerManager.getInstance().cannonBulletVelocity(this.level);
-        const angle = this.angleCannon - Math.PI / 2;
-        this.bullets.fire(this.x + this.parentContainer.x, this.y + this.parentContainer.y,
-            Math.cos(angle) * velocity, Math.sin(angle) * velocity);
-        SoundManager.getInstance().PlayerFire.play({ volume: 0.5 });
-    }
 }
 
 export class Modules extends Phaser.Physics.Arcade.Group {
