ab21388d8142993ce03f882e25fa3d3a1ce50913
PatBG <pat@patbgames.com>
2023-11-01 23:35:05 +0100

    Updated text intro with date of the post JAM version

--- src/SceneGameStart.ts
+++ src/SceneGameStart.ts
@@ -14,7 +14,7 @@ export class SceneGameStart extends Phaser.Scene {
     }
 
     create() {
-        this.textPos = new Phaser.Math.Vector2(GameManager.getInstance().canvasCenter.x, GameManager.getInstance().canvasCenter.y - 150);
+        this.textPos = new Phaser.Math.Vector2(GameManager.getInstance().canvasCenter.x, GameManager.getInstance().canvasCenter.y - 200);
 
         this.add.text(this.textPos.x, this.textPos.y - 50, "Shoot'n Trade",
             { font: '48px monospace', color: 'yellow' }).setOrigin(0.5);
@@ -22,6 +22,7 @@ export class SceneGameStart extends Phaser.Scene {
         this.textColor = 'yellow';
         this.addText('Made by PatBG for LUDUM DARE #54 (October 2023)');
         this.addText('Theme: "Limited Space"');
+        this.addText('Post JAM version (November 1, 2023)');
         this.addText('');
         this.addText('Phaser 3, TypeScript, Visual Studio Code,');
         this.addText('with the help of Phaser 3 tutorials and GitHub Copilot');
@@ -32,7 +33,6 @@ export class SceneGameStart extends Phaser.Scene {
         this.addText('Resell your merchandise farther to make profit and buy more space.');
         this.addText('');
         this.addText('[WASD, ZQSD, Arrow Keys or Mouse] to move ship');
-        this.addText('[Space, Left Shift or Mouse Button] to fire');
         this.addText('[P] to pause or resume the game');
         this.addText('');
         this.addText('[Press any key] to start');
