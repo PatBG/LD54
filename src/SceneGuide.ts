3d271f13fa3dc5b5dfa47bf977276d850c85005a
PatBG <pat@patbgames.com>
2023-11-20 23:57:11 +0100

    In admin mode, display guides at start

--- src/SceneGuide.ts
+++ src/SceneGuide.ts
@@ -1,4 +1,5 @@
 import * as Phaser from 'phaser';
+import { GameManager } from './GameManager';
 
 // Debug scene with resolutions guides 
 export class SceneGuide extends Phaser.Scene {
@@ -15,8 +16,10 @@ export class SceneGuide extends Phaser.Scene {
     }
 
     create() {
-        this.guide_640x720 = this.add.image(0, (1080 - 720) / 2, 'guide_640x720').setOrigin(0, 0).setDepth(1);
-        this.guide_512x1080 = this.add.image((640 - 512) / 2, 0, 'guide_512x1080').setOrigin(0, 0).setDepth(1);
+        this.guide_640x720 = this.add.image(0, (1080 - 720) / 2, 'guide_640x720').setOrigin(0, 0).setDepth(1).
+            setVisible(GameManager.getInstance().adminMode);
+        this.guide_512x1080 = this.add.image((640 - 512) / 2, 0, 'guide_512x1080').setOrigin(0, 0).setDepth(1).
+            setVisible(GameManager.getInstance().adminMode);
         this.input.keyboard.addKey('x').on('down', () => { this.hackShowGuide(); });
     }
 
